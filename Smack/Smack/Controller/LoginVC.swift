54ceab0fb1fd6c0ecdc4581192183206180527be
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-09-21 10:07:43 +0700

    LoginVC UI

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Trinh Thai on 9/21/19.
+//  Copyright © 2019 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func IBClosePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
