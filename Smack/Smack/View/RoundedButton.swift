7194f7aa2e3ef806db03b6364d4d9eaf4658ffdf
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-12-27 11:58:51 +0700

    Relayout

--- Smack/Smack/View/RoundedButton.swift
+++ Smack/Smack/View/RoundedButton.swift
@@ -12,7 +12,7 @@ import UIKit
 @IBDesignable
 class RoundedButton: UIButton {
 
-    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+    @IBInspectable var cornerRadius: CGFloat = 6.0 {
         didSet {
             self.layer.cornerRadius = cornerRadius
             //self.setNeedsLayout()
